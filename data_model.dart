092422b1cfbc7eaa489e1d2b7d852fd7059527b2
mediumsitompul <44793858+mediumsitompul@users.noreply.github.com>
2023-09-05 01:10:46 +0700

    Create data_model.dart

---
+++ data_model.dart
@@ -0,0 +1,46 @@
+class DataModel {
+  String? locId;
+  String? locX;
+  String? locY;
+  String? laundryName;
+  String? address;
+  String? phone;
+  String? status;
+
+  DataModel.fromJson(Map<String, dynamic> json) {
+    locId = json['id'].toString();
+    locX = json['lat'];
+    locY = json['lng'];
+    laundryName = json['laundry_name'];
+    address = json['address'];
+    phone = json['phone'];
+    status = json['status'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    return data;
+  }
+}
+
+class ListDataModel {
+  List<DataModel>? data;
+  ListDataModel({this.data});
+
+  ListDataModel.fromJson(Map<String, dynamic> json) {
+    if (json['data'] != null) {
+      data = <DataModel>[];
+      json['data'].forEach((v) {
+        data!.add(DataModel.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
